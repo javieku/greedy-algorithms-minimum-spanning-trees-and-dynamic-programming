9a87f43a8131950404c9fe4599081a60d8936b06
javieku <javieku@gmail.com>
2018-08-19 12:34:19 +0200

    Compute minimum and maximum distance among clusters

--- week2/clustering_with_union_find.rb
+++ week2/clustering_with_union_find.rb
@@ -13,6 +13,14 @@ class UnionFind
         leader_1, leader_2 = @leaders[id1], @leaders[id2]
         @leaders.map! {|i| (i == leader_1) ? leader_2 : i }
     end
+
+    def components
+      @leaders.group_by{|e| e}.map{|k, v| [k, v.length]}.delete_if { |k, v| k.nil? }
+    end
+
+    def component(id)
+      @leaders[id]
+    end
 end
 
 class Edge
@@ -25,15 +33,22 @@ class Edge
     @weight = weight
     end
     def to_s
-        "(From: " + @from.to_s + "To: " + @to.to_s + " W: " + @weight.to_s + ")"
+        "(From: " + @from.to_s + " To: " + @to.to_s + " W: " + @weight.to_s + ")"
     end
 end
 
 class ClusteringAlgorithm
-    attr_accessor :maximum_spacing
+    attr_accessor :max_spacing
+    attr_accessor :min_spacing
+
+    HIGH_INT = 999999
+    LOW_INT = -1
+    
     def initialize
-      @maximum_spacing = 111111111
+      @max_spacing = LOW_INT
+      @min_spacing = HIGH_INT
     end
+
     def execute(edges, n_of_nodes, clusters)
       
       set = UnionFind.new n_of_nodes
@@ -51,53 +66,82 @@ class ClusteringAlgorithm
         i += 1
       end
 
+      
+      # minimum distance among the cluster (max spacing)
       edges.each { |edg|  
         if !set.connected?(edg.from, edg.to)
-          puts edg
-          @maximum_spacing = [edg.weight,@maximum_spacing].min
+          @min_spacing = [edg.weight,@min_spacing].min
         end 
       }
+
+      # maximum distance among the clusters
+      spacings = {}
+      set.components.each do |key1,value1|
+        spacings[key1] = Hash.new(set.components.length)
+        set.components.each do |key2,value2|
+           spacings[key1][key2] = HIGH_INT if key1 != key2
+        end
+      end
+      edges.each { |edg|  
+        if !set.connected?(edg.from, edg.to)
+          leader_from = set.component(edg.from)
+          leader_to = set.component(edg.to)
+          spacings[leader_from][leader_to] = [spacings[leader_from][leader_to], edg.weight].min
+          spacings[leader_to][leader_from] = [spacings[leader_to][leader_from], edg.weight].min
+        end 
+      }
+      spacings.each do |key, values|
+        max_value = values.fetch(values.max_by{|k,v| v}.first)
+        @max_spacing = [max_value, @max_spacing].max
+      end
+
+      puts "Number of clusters -> #{set.components}" 
+      puts "Spacings -> #{spacings}" 
     end
 end
 
-  class Loader
-    include Singleton
-    def load_edges 
-      edges = []
-      File.open("clustering.txt", "r") do |f|
-          f.each_line do |line|
-            edge = line.split(/\s/).reject(&:empty?)
-            if edge.size < 2
-              next
-            end
-            from = edge[0].to_i
-            to = edge[1].to_i
-            weigth = edge[2]
-            edges.push(Edge.new(from, to, weigth.to_f))
+class Loader
+  include Singleton
+  def load_edges filename
+    edges = []
+    File.open(filename, "r") do |f|
+        f.each_line do |line|
+          edge = line.split(/\s/).reject(&:empty?)
+          if edge.size < 2
+            next
           end
+          from = edge[0].to_i
+          to = edge[1].to_i
+          weigth = edge[2]
+          edges.push(Edge.new(from, to, weigth.to_f))
         end
-      return edges
-    end
-    def load_n_of_nodes
-      n_of_nodes = 0
-      File.open('clustering.txt') {|f| n_of_nodes=f.readline.to_i}
-      return n_of_nodes
-    end
+      end
+      puts edges.length
+    return edges
   end
+  def load_n_of_nodes filename
+    n_of_nodes = 0
+    File.open(filename) {|f| n_of_nodes=f.readline.to_i}
+    return n_of_nodes
+  end
+end
   
-  def main
-    start = Time.now
-    edges = Loader.instance.load_edges
-    n_of_nodes = Loader.instance.load_n_of_nodes
-    puts "Edges loaded in memory " +  (Time.now - start).to_s
-    start = Time.now
-    puts "Starting clustering algorithm"
-    clustering = ClusteringAlgorithm.new
-    n_of_clusters = 2
-    clustering.execute(edges, n_of_nodes, n_of_clusters) 
-    puts "Clustering took " +  (Time.now - start).to_s
-    puts "Clustering result for #{n_of_clusters}"
-    puts clustering.maximum_spacing
-  end
+def main
+  start = Time.now
+  #data = "clustering-test-cases/clustering1-example-10-solution-11.txt"
+  data = "clustering.txt"
+  edges = Loader.instance.load_edges data
+  n_of_nodes = Loader.instance.load_n_of_nodes data
+  puts "Edges loaded in memory " +  (Time.now - start).to_s
+  start = Time.now
+  puts "Starting clustering algorithm"
+  clustering = ClusteringAlgorithm.new
+  n_of_clusters = 4
+  clustering.execute(edges, n_of_nodes, n_of_clusters) 
+  puts "Clustering took " +  (Time.now - start).to_s
+  puts "Clustering result for #{n_of_clusters}"
+  puts "Max distance among the clusters -> #{clustering.max_spacing}"
+  puts "Min distance among the clusters -> #{clustering.min_spacing}"
+end
 
-  main
+main
